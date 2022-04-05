47d788bc8b136d9c324f606d1ff422fc6179ac31
Cristian Tulbu <96737712+Cristian6918@users.noreply.github.com>
2022-04-05 17:49:25 +0200

    Init

---
+++ src/components/NewTitle.vue
@@ -0,0 +1,27 @@
+<template>
+<header>
+        <h1>La mia prima app con Vue CLI!</h1>
+        
+</header>
+    
+ 
+</template>
+
+<script>
+export default {
+  name: 'NewTitle',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1 {
+  margin: 40px 0 0;
+  color:red;
+}
+
+
+</style>
